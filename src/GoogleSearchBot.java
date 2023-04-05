d4a41ea7aa5ff46dd802d7a0ebb608ccae230f05
borhan <baris.orhan@hepsijet.com>
2023-04-05 15:23:33 +0300

    fix xml codes

--- src/GoogleSearchBot.java
+++ src/GoogleSearchBot.java
@@ -4,7 +4,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -17,22 +16,19 @@ public class GoogleSearchBot {
      * Kullanıcıdan String olarak Location inputu alınacak.X
      * GoogleSearchBot classının içerisine method olarak yazılacak.X
      * */
-    ArrayList<String> RainList = new ArrayList<String>();
-    ArrayList<String> HumidityList = new ArrayList<String>();
 
     public String Location() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please Write Your City: ");
-        String InputLocation = sc.nextLine();
+        String inputLocation = sc.nextLine();
         sc.close();
-        return InputLocation;
+        return inputLocation;
     }
 
     public void SearchBot(String Location) throws IOException { //Must be added Jsoup jar file
 
         final String GOOGLE_SEARCH_URL = "https://www.google.com/search";
         String searchURL = GOOGLE_SEARCH_URL + "?q=" + Location + "+hava&num=" + 1;
-
 
         Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
         Elements elements = doc.select("a");
@@ -42,46 +38,61 @@ public class GoogleSearchBot {
                 System.out.println("Weather URL:" + WeatherUrl);
                 searchURL = WeatherUrl;
             }
-
         }
         /*
          * weather.com... new searchurl
          */
-        Document weather = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
+        Document weather = Jsoup.connect(searchURL).userAgent("Chrome").get();
 
         //Temperature
-        Elements temps = weather.select("div.TodayDetailsCard--feelsLikeTemp--3fwAJ > span.TodayDetailsCard--feelsLikeTempValue--Cf9Sl");
-        for (Element e : temps) {
-            String TempFahrenheit = e.text();
-            System.out.println("Fahrenheit: " + TempFahrenheit);
-            TempFahrenheit = TempFahrenheit.replace("°", "");
-            double TempCelsius = Double.valueOf(TempFahrenheit);
-            TempCelsius = (TempCelsius - 32) * 0.5556;
-            System.out.println("Celsius: " + TempCelsius);
-        }
+        getTemperature(weather);
 
         //Rains
-        Elements Rains = weather.select("div.Column--precip--2ck8J > span.Column--precip--2ck8J");
-        for (Element e : Rains) {
-            String RainRate = e.text();
-            RainRate=RainRate.replace("Chance of Rain","");
-            RainList.add(RainRate);// Fifth rain rate is today's rate
-        }
-        System.out.println("Chance of Rain: "+ RainList.get(4));
+        getChanceOfRain(weather);
 
         //Humidity
-        Elements Humidity = weather.select("div.WeatherDetailsListItem--wxData--2s6HT");
-        for (Element e : Humidity) {
-            String HumidityPercentage = e.text();
-            HumidityList.add(HumidityPercentage);// 3th rate is today's rate
-        }
-        System.out.println("Percentage of Humidity: "+ HumidityList.get(2));
+        getPercentageOfHumidity(weather);
 
         //Wind
-        String WindPower= HumidityList.get(1);
-        WindPower=WindPower.replace("Wind Direction ","");
-        System.out.println("Wind Power: "+WindPower);
-
+        getWindPower(weather);
     }
 
+    public void getTemperature(Document weather){
+        Element todayDetails = weather.getElementById("todayDetails");
+        Elements details = todayDetails.select("[class*=TodayDetailsCard]");
+        Element temp = details.select("[class*=feelsLikeTempValue]").first();
+        if (temp != null){
+            String tempFahrenheit = temp.text();
+            System.out.println("Fahrenheit: " + tempFahrenheit);
+            String degree = tempFahrenheit.substring(tempFahrenheit.length()-1);
+            tempFahrenheit= tempFahrenheit.substring(0,tempFahrenheit.length()-1);
+            double TempCelsius = Double.valueOf(tempFahrenheit);
+            TempCelsius = (TempCelsius - 32) * 0.5556;
+            System.out.println("Celsius: " + TempCelsius+ degree);
+        }
+    }
+
+    public void getChanceOfRain(Document weather){
+        Elements chanceOfRainHTMLs = weather.select("span:contains(Chance of Rain)");
+        Element chanceOfRainHTML=chanceOfRainHTMLs.select("span:contains(%)").get(5);
+        String chanceOfRain=chanceOfRainHTML.text().substring(14);
+        System.out.println("Chance of Rain: "+ chanceOfRain);
+    }
+
+    public void getPercentageOfHumidity(Document weather){
+        Element humidity = weather.select("[data-testid*=PercentageValue]").first();
+        String humidityPercentage = humidity.text();
+        System.out.println("Percentage of Humidity: "+ humidityPercentage);
+    }
+
+    public void getWindPower(Document weather){
+        Element wind = weather.select("[data-testid*=Wind]").first();
+        String windPowerAll= wind.text();
+        String windPower=windPowerAll.replace("Wind Direction ","");
+        System.out.println("Wind Power(mp/h): "+windPower);
+        String windPowerMpH= windPower.substring(0,windPower.length()-3);
+        double windPowerKmH= Double.valueOf(windPowerMpH);
+        windPowerKmH = windPowerKmH*1.609344;
+        System.out.println("Wind Power(km/h): "+windPowerKmH + " kmh");
+    }
 }
